98b68601fe47030a9b2604bded9ea5348ecfe133
Quentin_bubu <Quentin_bubu@users.noreply.github.com>
2020-09-24 21:56:25 +0200

    CREATE character create
    Now, we can create a new character!

---
+++ assets/functions/character_create.py
@@ -0,0 +1,41 @@
+import os
+def character_create():  # creation assistée d'un personnage
+    if not os.path.exists("assets/character/"): # si le dossier personnages a été supprimé
+        os.makedirs("assets/character/") # on le recréé
+
+    print("Assistance de création d'un personnage")
+
+    elements = {
+        'name': '',
+        'start_heart': 0,
+        'heart': 0,
+        'attack': 0,
+        'dodge': 0,
+        'dodge_is_charge': 0,
+        'treatment_number': 0,
+        'treatment': 0,
+        'special_number': 0,
+        'special_attack': 0,
+        'special_attack_is_charge': 0,
+        'event': 'Aucun evenement'
+    }
+
+    elements['name'] = input("Saisissez un nom: ")
+    elements['start_heart'] = elements['heart'] = int(input("Saisissez ses points de vies: "))
+    elements['attack'] = int(input("Saisissez ses points d'attaque: "))
+    elements['dodge'] = int(input("Saisissez le nombre d'esquives possible: "))
+    elements['traitement_number'] = int(input("Saisissez le nombre de soin qui lui sera possible d'utiliser: "))
+    elements['traitement'] = int(input("Saisissez le nombre de points de vie regagnés lors du soin: "))
+    elements['special_number'] = int(input("Saisissez le nombre de coup spécial qu'il lui sera possible d'utiliser: "))
+    elements['special_attack'] = int(input("Saisissez les points de vies qui seront retirés lors du coup spécial : "))
+
+    file = open(f"assets/character/{elements['name']}.txt", "w")
+    file.write(str(elements))
+    file.close()
+
+    character_file = open("assets/character/character_list.txt", "r+")
+    character_list = character_file.readline()
+    character_list += ',' + elements['name']
+    character_file.write(character_list)
+    character_file.close()
+    input("Votre personnage à bien été créer!")
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from assets.Class.character import Character
 from assets.functions.clean import clean
 from assets.functions.file_remove import file_remove
+from assets.functions.character_create import character_create
 
-file_remove("assets/character/")
+character_create()
