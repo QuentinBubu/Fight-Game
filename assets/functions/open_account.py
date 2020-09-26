df5a6ea579b98e041fa797f7510cb92f9d22ae9e
QuentinBubu <Quentin_bubu@users.noreply.github.com>
2020-09-26 20:35:15 +0200

    Bug correction
    Now, the list is transform to string

--- assets/functions/open_account.py
+++ assets/functions/open_account.py
@@ -6,7 +6,7 @@ def open_account():
         os.makedirs("assets/accounts/")
     while True:
         clean()
-        account = input(f"Quel est ton compte?\n{os.listdir('assets/accounts/')}")
+        account = input(f"Quel est ton compte?\n{''.join(map(str, os.listdir('assets/accounts/'))).replace('.txt', '')}\n")
         if os.path.isfile(f"assets/accounts/{account}.txt"):
             return account
         print("Erreur")
