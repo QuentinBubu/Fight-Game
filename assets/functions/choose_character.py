ab93994b80e3fa12ee9e8d8a43833e2781f12c02
QuentinBubu <Quentin_bubu@users.noreply.github.com>
2020-09-26 23:57:41 +0200

    ADD choose character function
    Now, we can choose a character, but, this is not finishe, the character choose isn't lied with a new class

---
+++ assets/functions/choose_character.py
@@ -0,0 +1,23 @@
+import time
+import pickle
+from assets.functions.clean import clean
+
+def choose_character(user):
+    clean()
+    character_file = open("assets/character/character_list.txt", "r")
+    character_list  = list(character_file.readline().split(","))
+    character_file.close()
+    nomchoisi_tmp = 0
+
+    while nomchoisi_tmp not in character_list:
+        clean()
+        print(f"{user['username']}, choisis ton personnage: {''.join(map(str, character_list)).replace(',', ', ')}")
+        time.sleep(2)
+        for i in range(len(character_list)):
+            file = open(f"assets/character/{character_list[i]}.txt", "rb")
+            character_data = pickle.load(file)
+            print("--------------------------------")
+            print(f"Nom: {character_data['name']}\nVies: {character_data['heart']}\nAttaque: {character_data['attack']}\nNombre d'esquives: {character_data['dodge']}\nNombre de potion de soin: {character_data['traitement_number']}\nVies regagnées lors d'un soin: {character_data['traitement']}\nNombres de coup spéciaux: {character_data['special_number']}\nPoints de vies retirés lors d'une attaque spécial: {character_data['special_attack']}")
+            file.close()
+        character_choose = input()
+    return character_choose
