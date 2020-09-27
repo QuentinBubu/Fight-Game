b945523e5f63bc9b63e171f448475bf8459e2c4d
QuentinBubu <anagamesprint@gmail.com>
2020-09-27 21:28:50 +0200

    BUG: correction

--- assets/functions/choose_character.py
+++ assets/functions/choose_character.py
@@ -13,7 +13,7 @@ def choose_character(user):
         clean()
         print(f"{user['username']}, choisis ton personnage: {''.join(map(str, character_list)).replace(',', ', ')}")
         time.sleep(2)
-        for i in range(len(character_list)):
+        for i in range(1,len(character_list)):
             file = open(f"assets/character/{character_list[i]}.txt", "rb")
             character_data = pickle.load(file)
             print("--------------------------------")
