27f289b932e5f9ffc5270c5c1dde5bedf77f49da
QuentinBubu <anagamesprint@gmail.com>
2020-09-28 21:28:58 +0200

    Update choose_character.py

--- assets/functions/choose_character.py
+++ assets/functions/choose_character.py
@@ -17,7 +17,7 @@ def choose_character(user):
             file = open(f"assets/character/{character_list[i]}.txt", "rb")
             character_data = pickle.load(file)
             print("--------------------------------")
-            print(f"Nom: {character_data['name']}\nVies: {character_data['heart']}\nAttaque: {character_data['attack']}\nNombre d'esquives: {character_data['dodge']}\nNombre de potion de soin: {character_data['traitement_number']}\nVies regagnées lors d'un soin: {character_data['traitement']}\nNombres de coup spéciaux: {character_data['special_number']}\nPoints de vies retirés lors d'une attaque spécial: {character_data['special_attack']}\n")
+            print(f"Nom: {character_data['name']}\nVies: {character_data['heart']}\nAttaque: {character_data['attack']}\nNombre d'esquives: {character_data['dodge']}\nNombre de potion de soin: {character_data['traitement_number']}\nVies regagnées lors d'un soin: {character_data['traitement']}\nNombres de coup spéciaux: {character_data['special_number']}\nPoints de vies retirés lors d'une attaque spécial: {character_data['special_attack']}")
             file.close()
         character_choose = input()
     return character_choose
