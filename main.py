5d2f8193e5f846869a026eca9e110d6769aa4485
QuentinBubu <anagamesprint@gmail.com>
2020-10-03 12:03:20 +0200

    BUG: correction
    The game has an end!

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from assets.functions.pre_start import pre_start
 from assets.functions.settings import settings
 
 action_list = ["attaque", "esquive", "soin", "charge", "attaque spéciale"]
+action_list2 = ["attaque", "esquive", "soin", "charge", "attaque spéciale", "attaque speciale"]
 error = 0
-
-#try:
 
 print("Bienvenue dans Fight Game, un super jeu de combat!")
 time.sleep(2)
@@ -34,9 +33,11 @@ for i in range(3,0,-1):
 print("C'est parti!")
 time.sleep(1)
 
-while player1.heart > 0 or player2.heart > 0:
+player1.heart = player2.heart = 25
+
+while player1.heart > 0 and player2.heart > 0:
     i = 0
-    while i < 2:
+    while i < 2 and (player1.heart > 0 and player2.heart > 0):
         clean()
         if i == 0:
             player = player1
@@ -45,12 +46,14 @@ while player1.heart > 0 or player2.heart > 0:
             player = player2
             opponent = player1
 
+        print(player1.heart, "...", player2.heart)
+
         input(f"{player.username} c'est à toi, appuis sur \"Entrée\"") if not error else 0
 
         player.dodge_is_charge = False
 
         action = 0
-        while not action in action_list:
+        while not action in action_list2:
             clean()
             print(f"Dernier événement: {player.event}\n")
             print(f"Vies restantes: {player.heart}\nAttaque: {player.attack}\nNombre d'esquives restantes: {player.dodge}\nNombre de potion de soin restantes: {player.treatment_number}\nVies regagnées lors d'un soin: {player.treatment}\nNombres de coup spéciaux restants: {player.special_number}\nPoints de vies retirés lors d'une attaque spécial: {player.special_attack}\nAttaque spécial chargée: {player.special_attack_is_charge}\n\nVies restantes de l'adversaire: {opponent.heart}")
@@ -80,30 +83,15 @@ while player1.heart > 0 or player2.heart > 0:
                 input(error)
                 continue
 
-        elif action == "attaque spéciale":
+        elif action == "attaque spéciale" or "attaque speciale":
             error = player.f_special_attack(opponent)
             if error:
                 input(error)
                 continue
 
-        if player1.heart < 0 or player2.heart < 0:
-            break
-
         i += 1
 
 else:
-    pass
-
-"""
-except Exception as erreur:
-    Rapport_Bug = str(Rapport_Bug) + repr(erreur) + "fin"
-    print("Nous sommes désolé mais une erreur que nous n'avons pas pu traitée est surenue! Veuillez relancer le jeu! Toutes nos excuses,")
-    a = input("Voulez vous crée un rapport bug et l'envoyer par mail? o/n")
-    if a == "o":
-        if not os.path.exists("Rapport/"):
-            os.makedirs("Rapport/")
-        fichier = open("Rapport/Rapport_bug.txt", "w+")
-        fichier.write(str(Rapport_Bug))
-        fichier.close
-        print("Merci! le rapport a été créer dans le dossier Rapport dans les fichiers de l'appli! Veuillez l'envoyer à anagamesprint@gmail.com; Merci!")
-"""
+    clean()
+    winner = player2.username if player1.heart <= 0 else player1.username
+    input(f"Fin du jeu!!\nBravo à {winner} qui a gagné!")
