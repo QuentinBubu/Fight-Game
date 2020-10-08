bb4705717e08cb3d740fdcdfc07774b9045855c2
QuentinBubu <anagamesprint@gmail.com>
2020-10-08 22:35:55 +0200

    BUG: correction
    Correct the path of files.

--- assets/functions/open_game.py
+++ assets/functions/open_game.py
@@ -11,8 +11,8 @@ def open_game():
     global player1, player2, turn, game_already_open
 
     print("Quel partie voulez vous ouvrir?")
-    game_to_open = input(f"{str(os.listdir('assets.game_saved')).replace('[', '').replace(']', '').replace(',', ', ')}")
-    file = open(game_to_open, "rb")
+    game_to_open = input(f"{str(os.listdir('assets/game_saved/')).replace('[', '').replace(']', '').replace(',', ', ').replace('.txt', '')}\n")
+    file = open(f"assets/game_saved/{game_to_open}.txt", "rb")
     game_date = pickle.load(file)
     file.close()
 
